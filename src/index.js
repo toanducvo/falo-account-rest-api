54bbff415558e23cb89680ce7bb1ad43d09cf38c
ductoanvo <hey.toan@outlook.com>
2021-11-08 22:46:17 +0700

    Use cors middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const routes = require("./routes");
 const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 require("dotenv").config({ path: ".env.development" });
 
 const app = express();
@@ -10,6 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   // Predefined request id
   // Add request id to the request object
